640d6c52a410cd214b837f86bdcc09eecc76597c
Bonface Mudiri <mudirib@gmail.com>
2019-12-22 10:52:06 -0800

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world</h1>
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Contacts</li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
